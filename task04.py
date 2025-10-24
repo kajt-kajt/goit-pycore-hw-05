dcf445f060e15659a9060b646197a78155d3d576
kajt <kajt.kajt.kajt.kajt.kajt@gmail.com>
2025-10-25 00:08:32 +0300

    task 4 complete

--- task04.py
+++ task04.py
@@ -1,3 +1,24 @@
+from functools import wraps
+from typing import Callable
+from collections import defaultdict
+
+# handler decorator
+def input_error(func: Callable) -> Callable:
+    """
+    Decorator to handle typical errors caused by wrong user input.
+    """
+    @wraps(func)
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (ValueError, IndexError):
+            return "Wrong argument(-s) provided. Try again."
+        except KeyError:
+            return "No such contact found."
+    return inner
+
+# handlers
+@input_error
 def parse_input(user_input: str) -> tuple[str,list[str]]:
     """
     Parses user input into command and arguments.
@@ -6,11 +27,12 @@ def parse_input(user_input: str) -> tuple[str,list[str]]:
     cmd = cmd.strip().lower()
     return cmd, *args
 
+@input_error
 def add_contact(args: list[str], contacts: dict[str, str]) -> str:
     """
     Adds new entry to contacts dictionary. 
     Returns a warning if contact with such name already exists, but anyway rewrites it.
-    "args" should contain 2 values, it is guaranteed by caller.
+    "args" should contain 2 values.
     """
     warning = ""
     name, phone = args
@@ -19,11 +41,12 @@ def add_contact(args: list[str], contacts: dict[str, str]) -> str:
     contacts[name] = phone
     return warning + "Contact added."
 
+@input_error
 def change_contact(args: list[str], contacts: dict[str,str]) -> str:
     """
     Updates existing contact with new phone value.
     Returns an error message if contact with given name does not exist.
-    "args" should contain 2 values, it is guaranteed by caller.
+    "args" should contain 2 values.
     """
     name, phone = args
     if name not in contacts:
@@ -32,17 +55,16 @@ def change_contact(args: list[str], contacts: dict[str,str]) -> str:
         contacts[name] = phone
         return "Contact updated."
 
-def show_phone(name: str, contacts: dict[str: str]) -> str:
+@input_error
+def show_phone(args: list[str], contacts: dict[str: str]) -> str:
     """
     Returns phone for given name.
     Returns an error message if contact with such name is absent.
     """
-    if name not in contacts:
-        return f"ERROR: contact '{name}' does not exist!"
-    else:
-        return contacts[name]
+    name = args[0]
+    return contacts[name]
 
-def show_all(contacts: dict[str, str]) -> str:
+def show_all(_, contacts: dict[str, str]) -> str:
     """
     Outputs all the contents of in-memory database of contacts.
     """
@@ -51,29 +73,36 @@ def show_all(contacts: dict[str, str]) -> str:
         result.append(f"{name}: {phone}")
     return "\n".join(result)
 
+
 def main():
+    # storage for contacts
     contacts = {}
+
+    # command handlers
+
+    def default_handler():
+        def inner(*args, **kwargs):
+            return "Invalid command."
+        return inner
+    
+    # all handlers should take 2 arguments - args list and contacts dictionary
+    handlers = defaultdict(default_handler)
+    handlers["hello"] = lambda x,y: "How can I help you?"
+    handlers["close"] = lambda x,y: "Good bye!"
+    handlers["exit"] = lambda x,y: "Good bye!"
+    handlers["add"] = add_contact
+    handlers["change"] = change_contact
+    handlers["phone"] = show_phone
+    handlers["all"] = show_all
+
     print("Welcome to the assistant bot!")
-    while True:
+
+    # main loop
+    command = ""
+    while command not in ["close", "exit"]:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
-
-        if command in ["close", "exit"] and len(args) == 0:
-            print("Good bye!")
-            break
-        elif command == "hello" and len(args) == 0:
-            print("How can I help you?")
-        elif command == "add" and len(args) == 2:
-            print(add_contact(args, contacts))
-        elif command == "change" and len(args) == 2:
-            print(change_contact(args, contacts))
-        elif command == "phone" and len(args) == 1:
-            print(show_phone(args[0], contacts))
-        elif command == "all" and len(args) == 0:
-            print(show_all(contacts))
-        else:
-            # if command is unknown or number of arguments is wrong
-            print("Invalid command.")
+        print(handlers[command](args, contacts))
 
 if __name__ == "__main__":
     main()
